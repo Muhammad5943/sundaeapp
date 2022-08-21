0fce8707279a99e08d4641a8d4686ecd6f33c8ba
Muhammad5943 <muhammad.aji.putra.mp98@gmail.com>
2022-08-21 10:23:44 +0700

    SetUp: Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['sofia-pro', 'Helvetica', 'Arial', 'sans-serif']
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
